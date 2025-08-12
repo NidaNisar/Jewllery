d2a4c3cec41e0833ba834ca9d1fe5324150f8f70
nidanisar118@gmail.com <nidanisar118@gmail.com>
2025-08-12 19:34:02 +0500

    forget page style

--- src/components/forget/Forget.js
+++ src/components/forget/Forget.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import './forget.css'
+import Footer from '../../components/footer/Footer'
 const Forget = () => {
   return (
     <div>
         <div className='forget-container'>
-            <p>Forgot your Password</p>
+          <div className='forgetp'>
+             <p className='forget-pass'>Forgot your Password</p>
             <p>We wil send you an email to reset your Password</p>
-           <form>
+          </div>
+           
+           <form className='forget-form'>
                 <label for="email">Email</label>
-                   <input type="email" id="email" name="email" className='forget-email' placeholder='Enter your e-mail'/>
+                   <input type="email" id="email" name="email" className='forget-email' placeholder='Enter your e-mail'
+                   required />
                    </form>
-                   <div className='forget-buttons'>
-                   <button className='forget'>Submit</button>
-                   <button className='forget'>Cancel</button>
+                 
                    </div>
-        </div>
+                     <div className='forget-buttons'>
+                   <button className='forgot'>Submit</button>
+                   <button className='forgot'>Cancel</button>
+                              </div>
+                              <Footer/>
     </div>
   )
 }
