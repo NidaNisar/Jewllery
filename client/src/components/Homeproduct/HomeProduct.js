8b7f32b4cb0c528d76510179e8a226a2f8c88565
nidanisar118@gmail.com <nidanisar118@gmail.com>
2025-09-21 05:01:22 +0500

    Organize frontend into client/ and add backend server

---
+++ client/src/components/Homeproduct/HomeProduct.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './Homeproduct.css'
+
+import { Link } from 'react-router-dom'
+import {Products} from '../../productjson'
+
+
+
+
+const HomeProduct = () => {
+  return (
+    <div className='main-home'>
+        <h3>Our Most Be Loved Product</h3>
+        
+        
+          
+      
+            <div className='forproducts'>
+          {Products.map(product=>{
+        return(
+            <Link to={`/productcart/${product.id}`}>
+              <div  key={product.id} className='all'>
+           < div className='home-product'>
+               <img alt='' src={product.image}/>
+      </div>
+      <div className='homep-name'>
+               <p>{product.name}</p>
+               <i class="fa-regular fa-star"></i>
+               <i class="fa-regular fa-star"></i>
+               <i class="fa-regular fa-star"></i>
+               <i class="fa-regular fa-star"></i>
+                <p> Rs. {product.price}</p>   
+      </div>
+      </div></Link>
+      
+        )
+    
+      })}
+      </div>
+   
+      
+     
+  
+     
+       
+      
+    </div>
+    
+  )
+}
+
+export default HomeProduct
---
+++ client/src/productjson.js
@@ -0,0 +1,40 @@
+import main6 from './pictures/main6.jpg'
+import main8 from './pictures/main8.jpeg'
+import main9 from './pictures/main9.jpeg'
+import main10 from './pictures/main10.jpeg'
+import main2 from './pictures/main2.jpg'
+import main4 from './pictures/main4.jpg'
+import main3 from './pictures/main3.jpg'
+import main7 from './pictures/main7.jpg'
+import main1 from './pictures/main1.jpg'
+import main11 from './pictures/main11.jpg'
+import main12 from './pictures/main12.jpg'
+import main13 from './pictures/main13.jpg'
+
+
+
+
+export  const Products=[
+  {id:'2',name:'Earings',price:'1000',image:main11,categoryid:21,quantity:1},
+  {id:'1',name:'Necklace',price:'1000',image:main10,categoryid:20,quantity:1},
+        {id:'3',name:'Rings',price:'1000',image:main12,categoryid:22,quantity:1},
+          {id:'4',name:'Ring',price:'1000',image:main13,categoryid:22,quantity:1},
+            {id:'5',name:'Earrings',price:'1000',image:main1,categoryid:21,quantity:1},
+              {id:'6',name:'Earrings',price:'1000',image:main7,categoryid:21,quantity:1},
+                {id:'7',name:'Earrings',price:'1000',image:main3,categoryid:21,quantity:1},
+                  {id:'8',name:'Locketset',price:'1000',image:main4,categoryid:23,quantity:1},
+                    {id:'9',name:'Earrings',price:'1000',image:main2,categoryid:21,quantity:1},
+                      {id:'10',name:'Watches',price:'1000',image:main6,categoryid:24,quantity:1},
+                        {id:'11',name:'Rings set',price:'1000',image:main8,categoryid:25,quantity:1},
+                          {id:'12',name:'Braclets',price:'1000',image:main9,categoryid:26,quantity:1}
+
+]
+ export const categories = [
+  { id: 21, name: 'Earings' },
+  { id: 20, name: 'Necklaces' },
+  { id: 22, name: 'Rings' },
+  {id:26,name:'Rings set'},
+  {id:24,name:'Watches'},
+  {id:23,name:'locket set'},
+
+];
