d7413a0fe047b5d12d81a1876ee45227573b8ecd
nidanisar118@gmail.com <nidanisar118@gmail.com>
2025-09-21 22:51:36 +0500

    Creating user model

--- serverr/models/userModel.js
+++ serverr/models/userModel.js
@@ -2,6 +2,27 @@ const mongoose=require('moongoose');
 const{model,Schema}=require('moongose');
 
 const userSchema=new Schema ({
+    Firstname:{
+        type:String,
+        require:[true,"Please enter your name"]
+
+    },
+     Secondname:{
+        type:String,
+        require:[true,"Please enter your name"]
+     },
+    email:{
+        type:String,
+        require:[true,"Please enter your email"],
+        unique:true,
+        lowercase:true,
+        validator:[validator.isEmail,"Please enter a valid e-mail"]
+    },
+    password:{
+        type:String,
+          require:[true,"Please enter your Password"],
+          minlength:8
+    }
 
 })
 
