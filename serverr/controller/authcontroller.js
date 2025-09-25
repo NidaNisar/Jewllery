d43b42b3f8fe5ef5c334f5f3230a0ad169b3d0dd
nidanisar118@gmail.com <nidanisar118@gmail.com>
2025-09-25 21:46:27 +0500

    acessing the protecting routes

--- serverr/controller/authcontroller.js
+++ serverr/controller/authcontroller.js
@@ -1,4 +1,6 @@
 const User=require("./../models/userModel");
+const util = require("util");
+
 const jwt =require('jsonwebtoken');
 const bcrypt = require("bcryptjs");
 
@@ -6,6 +8,28 @@ const bcrypt = require("bcryptjs");
     return jwt.sign({id},process.env.SECRET_STR,{
             expiresIn:process.env.LOGIN_EXPIRES
            }) 
+ }
+ const getalluser=async(req,res,next)=>{
+    try {
+         const alluser= await User.find();
+         res.status(200).json({
+            success:true,
+            lenght:alluser.lenght,
+            data:{
+                alluser:alluser
+            }
+
+         })
+    } catch (error) {
+        res.status(400).json({
+            success:false,
+            message:"Server error",
+            err:error.message
+
+         })
+    }
+    next();
+   
  }
 const createuser= async(req,res,next)=>{
     try {
@@ -16,9 +40,11 @@ const createuser= async(req,res,next)=>{
             success:true,
             message:"User is created",
            data:{
-                user:newuser
+                user:newuser,
+                
             }
         })
+        next();
     } catch (error) {
         
           res.status(400).json({
@@ -55,6 +81,7 @@ const login= async(req,res,next)=>{
             message:"Logged in",
            token:''
         })
+        next();
     } catch (error) {
         
           res.status(400).json({
@@ -64,5 +91,45 @@ const login= async(req,res,next)=>{
            
         })
     }
+    
 }
-module.exports={createuser,login};
+
+const protect= async (req,res,next)=>{
+// 1 Read the token and  check if exist
+try {
+    const testtoken=req.headers.authorization;
+ let token;
+ if(testtoken && testtoken.startsWith('bearer'))
+ {
+    token=testtoken.split(' ')[1]
+    
+
+ }
+ console.log(token)
+ if(!token){
+    return res.status(400).json({
+        message:"you are not loggeg inn"
+    })
+ }
+  
+ // 2 Validate the token
+
+  const decodedtoken=  await util.promisify(jwt.verify)(token,process.env.SECRET_STR)
+  // 3 If the user exists
+     const user=await User.findById(decodedtoken)
+     if(!user)
+     {
+        res.status(401).json({
+             message:"The user is not exist "
+        })
+     }
+
+ next();
+} catch (error) {
+    return res.status(401).json({
+      message: "Invalid or expired token. Login again",
+    });
+}
+ 
+}
+module.exports={createuser,login,protect,getalluser};
--- serverr/routes/authroute.js
+++ serverr/routes/authroute.js
@@ -1,7 +1,8 @@
 const express=require("express");
 const router=express.Router();
-const {createuser,login}=require("./../controller/authcontroller");
+const {createuser,login, getalluser, protect}=require("./../controller/authcontroller");
 
 router.post("/auth", createuser )
 router.post("/login",login)
+router.get("/getusers", protect,getalluser)
 module.exports=router;
